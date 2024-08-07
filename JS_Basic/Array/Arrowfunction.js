35a85bc7a137e4ba754f79dd5add06d14f88841b
Devu-Pari <maliatulpandharpur@gmail.com>
2024-08-07 07:58:28 +0530

    Arrowfunction example

---
+++ JS_Basic/Array/Arrowfunction.js
@@ -0,0 +1,44 @@
+//sorting
+
+let a=[3,2,5,7,1,9,5];
+let val=a.sort().reverse();
+console.log(val);
+
+// let rev=a.reverse();
+// console.log(rev);
+
+
+//map()---.interact with each element in an array
+
+//filter() ----> filtering element of an array
+
+//reduce()---> reduce the size of an array by 
+//flat()--combining array in singele array
+
+
+// array
+
+// pre -- number, boolean und, string
+//ref ---> array, object, function
+
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// let d = 40;
+// const e = 100; 
+// e = 100;
+
+
+const val = [2, 4, 3, 1, 5]; //1000
+//0  1  2                     
+// values = 1000;
+// values = [2,2,3,4] //3000
+// values = 3000;
+// const a = ['abc', 11, true, null, undefined, NaN, { name: "Abc" }, function () { }];
+
+// const f1 = a[8];
+// f1();
+// console.log(values[1])
+// console.log(values[0])
+
+// values[1] = 22;
