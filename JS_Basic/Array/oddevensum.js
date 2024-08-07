8382dc8c2f61f5286ad2a8ba8239b9e94bb33612
Devu-Pari <maliatulpandharpur@gmail.com>
2024-08-07 10:41:00 +0530

    code reduce odd even

--- JS_Basic/Array/oddevensum.js
+++ JS_Basic/Array/oddevensum.js
@@ -1,18 +1,11 @@
 
 const a=[1,2,3,4,5];
-
 let sum=0;
-
 for(let i=0;i<a.length;i++){
     if(a[i]%2==1)
      sum=sum+a[i];
 
 }
 console.log(sum);
-
-
 const sqr=a.map(ele=>ele*ele);
 console.log(sqr);
-
-//const max=a.reduce(e=>)
-
