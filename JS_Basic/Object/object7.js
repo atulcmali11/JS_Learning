a40e46ce10da717d2a764fc666c42ef9d75109b1
Devu-Pari <maliatulpandharpur@gmail.com>
2024-08-09 08:37:32 +0530

    has own property another program

--- JS_Basic/Object/object7.js
+++ JS_Basic/Object/object7.js
@@ -3,7 +3,7 @@ const obj={};
 
 for(let i = 0; i < str.length; i++){
     if (str[i] >= '0' && str[i] <= '9') {  // Check if character is a digit
-      if(obj[str[i]]){
+      if(obj.hasOwnProperty([str[i]])){
         obj[str[i]]= obj[str[i]]+1;
       }
       else{
